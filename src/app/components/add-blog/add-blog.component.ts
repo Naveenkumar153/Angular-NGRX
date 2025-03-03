c867b5ea311460cedb5491522ca7f2f76a312249
Naveenkumar153 <naveenkumartech2025@gmail.com>
2025-03-03 18:50:47 +0530

    Implement AddBlog component: update dialog for adding blog posts with title and description fields, enhance styling, and integrate state management for editing existing blogs

--- src/app/components/add-blog/add-blog.component.ts
+++ src/app/components/add-blog/add-blog.component.ts
@@ -1,23 +1,45 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
 import { MaterialModule } from 'src/app/material.module';
+import { BlogModel } from 'src/app/store/Blog/Blog.model';
+import { getBlogBasedOnId } from 'src/app/store/Blog/Blog.selector';
 
 @Component({
   selector: 'app-add-blog',
   templateUrl: './add-blog.component.html',
   styleUrls: ['./add-blog.component.scss'],
   standalone: true,
-  imports:[MaterialModule],
+  imports:[MaterialModule,FormsModule],
 })
 export class AddBlogComponent implements OnInit{
 
-  constructor(){}
+  title!: string;
+  description!: string;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public matData:{ title: string, data:number },
+    public dialogRef: MatDialogRef<AddBlogComponent>,
+    private store: Store
+  ){}
 
   ngOnInit(): void {
+    console.log(this.matData);
+    if(this.matData.data){
+      this.store.select(getBlogBasedOnId(this.matData.data)).subscribe((blog: BlogModel | null) => {
+        blog && ( this.title = blog.title,  this.description = blog.description);
+      });
+    }
   }
 
-  openDialog(){
-    
+  saveBlog():void{
+    this.dialogRef.close({
+      id: Math.floor(Math.random()),
+      title: this.title,
+      description: this.description,
+    });
   }
 
 }
