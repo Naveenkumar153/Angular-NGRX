810dd5ac05acb4092409078cd296c13426417a8c
Naveenkumar153 <naveenkumartech2025@gmail.com>
2025-03-05 19:37:44 +0530

    Add comments section to home component with input and display functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { CounterComponent } from 'src/app/components/counter/counter.component';
 import { NavBarModule } from 'src/app/components/nav-bar/nav-bar.module';
@@ -9,8 +10,16 @@ import { NavBarModule } from 'src/app/components/nav-bar/nav-bar.module';
   standalone: true,
   imports:[
     NavBarModule,
+    NgFor
   ]
 })
 export class HomeComponent {
+  name:string = 'Naveen';
+  disabled = false;
 
+  comments:string[] = [];
+
+  addComment(comment:string){
+    this.comments.push(comment);
+  }
 }
