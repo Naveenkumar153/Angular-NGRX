fe3bb67adcce7cf668ad9e7b4f1179e8b0c17da1
Naveenkumar153 <naveenkumartech2025@gmail.com>
2025-03-22 21:48:02 +0530

    Implement OnInit lifecycle hook and add closure example in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { asapScheduler, asyncScheduler, observeOn, of, queueScheduler, Subject } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { asapScheduler, asyncScheduler, map, Observable, observeOn, of, queueScheduler, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngrx';
   sub = new Subject<'Subject hello'>();
   constructor(private http:HttpClient) {
@@ -20,8 +20,8 @@ export class AppComponent {
     Promise.resolve('Promise has resolved').then((value) => console.log(value)); // micro task
 
     of('Stream value').subscribe(console.log); // ??
-    // this.sub.pipe(observeOn(asapScheduler)).subscribe(console.log);
-    // this.sub.next('Subject hello');
+    this.sub.pipe(observeOn(asapScheduler)).subscribe(console.log);
+    this.sub.next('Subject hello');
 
     // write the mutation observer code here
   //  const mutation = new MutationObserver(() => console.log('MutationObserver has ended'));
@@ -35,8 +35,53 @@ export class AppComponent {
     // process.nextTick(() => console.log('process.nextTick has ended')); 
 
     // this.http.get("https://jsonplaceholder.typicode.com/todos/1").pipe(observeOn(asyncScheduler)).subscribe(console.log);
+
     of('asapScheduler value').pipe(observeOn(asapScheduler)).subscribe(console.log); // we can run even micro task
     of('asyncScheduler').pipe(observeOn(asyncScheduler)).subscribe(console.log); // we can run even macro task
     of('queueScheduler').pipe(observeOn(queueScheduler)).subscribe(console.log); // we can run even macro task
-  }
-}
+  };
+
+  ngOnInit(): void {
+      // this.hotAndColdObservables();
+      this.clousreExample();
+      this.clousreExample();
+  };
+
+  clousreExample() {
+    let counter = Date.now();
+    const increment = () => {
+      debugger;
+      console.log(counter);
+    };
+    increment();
+  };
+
+  hotAndColdObservables(){
+    // // cold observable
+    // const coldObservable = of('Cold observable');
+    // coldObservable.subscribe(console.log);
+    // coldObservable.subscribe(console.log);
+
+    // // hot observable
+    // const hotObservable = new Subject();
+    // hotObservable.next('Hot observable');
+    // hotObservable.subscribe(console.log);
+    // hotObservable.subscribe(console.log);
+
+
+    // const obs$ = formTimeStamp();
+    const obs$ = of(null).pipe(map(() => Math.random()));
+    obs$.subscribe(console.log);
+    obs$.subscribe(console.log);
+    setTimeout(() => {
+      obs$.subscribe(console.log);
+    },1000);
+  };
+};
+
+const formTimeStamp = ():Observable<number> => {
+  const timeStamp = Date.now();
+  return new Observable((subscriber) => {
+    subscriber.next(timeStamp);
+  });
+};
