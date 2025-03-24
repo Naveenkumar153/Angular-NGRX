67f7397b2d66409b4980b084a8bd144f479c9214
Naveenkumar153 <naveenkumartech2025@gmail.com>
2025-03-24 18:31:51 +0530

    Implement getAllBlogs method in MasterService with error handling and add corresponding unit tests

--- src/app/shared/services/master.service.spec.ts
+++ src/app/shared/services/master.service.spec.ts
@@ -1,16 +1,73 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MasterService } from './master.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { BlogModel } from '@src/store/Blog/Blog.model';
 
 describe('MasterService', () => {
   let service: MasterService;
-
+  let httpMock: HttpTestingController;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [MasterService],
+      imports:[HttpClientTestingModule]
+    });
     service = TestBed.inject(MasterService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+    expect(httpMock).toBeTruthy();
   });
+
+  it("should handle an all blogs list", async () => {
+    const url = 'http://localhost:3000/blogs';
+    const dummyBlogs = [
+      {
+        "id": 1,
+        "title": "Angular-55fasdffsfd",
+        "description": "Angular is a platform and framework for building single-page client applications using HTML and TypeScript."
+      },
+      {
+        "id": 2,
+        "title": "React",
+        "description": "React is a JavaScript library for building user interfaces."
+      },
+      {
+        "id": 3,
+        "title": "Vue",
+        "description": "Vue.js is a progressive framework for building user interfaces."
+      }
+    ]
+
+    await service.getAllBlogs().subscribe(data => {
+      console.log("data",data);
+      expect(data).toEqual(dummyBlogs);
+    });
+    const req = httpMock.expectOne(url);
+
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyBlogs);
+  });
+
+  it("should handle error while fetching data", async () => {
+    const url = 'http://localhost:3000/blogs';
+    const errorMsg = 'An error occurred';
+    
+    await service.getAllBlogs().subscribe({
+      next: () => fail('expected an error'),
+      error: (error: HttpErrorResponse) => {
+        expect(error).toBeTruthy();
+        expect(error?.message).toContain(errorMsg);
+      }
+    });
+    
+    
+    const req = httpMock.expectOne(url);
+    expect(req.request.method).toBe('GET');
+    req.error(new ErrorEvent('An error occurred', { message:errorMsg }));
+  });
+
 });
--- src/app/shared/services/master.service.ts
+++ src/app/shared/services/master.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { BlogModel } from "@src/store/Blog/Blog.model";
-import { Observable } from "rxjs";
+import { catchError, Observable, throwError } from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
@@ -24,5 +24,18 @@ constructor() { }
 
     deleteBlog(id:number):Observable<BlogModel> {
         return this.http.delete<BlogModel>(`${this.blogUrl}/${id}`);
+    };
+
+    getAllBlogs():Observable<BlogModel[]>{
+        return this.http.get<BlogModel[]>(this.blogUrl).pipe(catchError(this.handleError));
+    };
+
+    private handleError(error:HttpErrorResponse){
+        if(error.error instanceof ErrorEvent){
+            return throwError(() => new Error('An error occurred:', error.error.message));
+        }else{
+            return throwError(() => new Error(`Backend returned code ${error.status}, body was: ${error.error}`));
+        }
+        // return throwError(() => new Error(`'Something bad happened; please try again later.'`));
     }
 }
