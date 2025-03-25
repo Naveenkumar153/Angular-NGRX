a18bbc8aeb454d9a9965b65362d19e93bc06033f
Naveenkumar153 <naveenkumartech2025@gmail.com>
2025-03-25 14:04:28 +0530

    Add unit tests for BlogComponent to mock dependencies and verify action dispatch on initialization

--- src/app/pages/blog/blog.component.spec.ts
+++ src/app/pages/blog/blog.component.spec.ts
@@ -1,15 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BlogComponent } from './blog.component';
+import { of } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { blogsActions } from '@src/store/Blog/Blog.action';
 
-xdescribe('BlogComponent', () => {
+describe('BlogComponent', () => {
   let component: BlogComponent;
   let fixture: ComponentFixture<BlogComponent>;
 
-  beforeEach(() => {
+  let mockStore = {
+    select: jasmine.createSpy('select').and.returnValue(of([])),
+    dispatch: jasmine.createSpy('dispatch')
+  }; 
+
+  let mockDialog = {
+    open: jasmine.createSpy('open').and.returnValue({ afterClosed: of([]) }),
+  };
+
+  let mockSnackBar = {
+    open: jasmine.createSpy('open').and.returnValue({ afterClosed: of([]) }),
+  }
+
+  beforeEach( () => {
     TestBed.configureTestingModule({
-      declarations: [BlogComponent]
-    });
+      imports: [BlogComponent],
+      providers:[
+        { provide:Store, useValue:mockStore },
+        { provide:MatDialog, useValue: mockDialog },
+        { provide:MatSnackBar, useValue: mockSnackBar },
+      ],
+    }).compileComponents();
     fixture = TestBed.createComponent(BlogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +40,9 @@ xdescribe('BlogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch load blogs action on init', () => {
+    expect(mockStore.dispatch).toHaveBeenCalled()
   });
 });
