5bfe8c71b269d4ef66b130a30337b66c1576050f
Naveenkumar153 <naveenkumartech2025@gmail.com>
2025-03-25 14:04:33 +0530

    Add unit tests for PermissionDirective to validate permission handling and element removal

--- src/app/shared/directives/permission.directive.spec.ts
+++ src/app/shared/directives/permission.directive.spec.ts
@@ -1,8 +1,50 @@
+import { TestBed } from '@angular/core/testing';
 import { PermissionDirective } from './permission.directive';
+import { ElementRef, Renderer2 } from '@angular/core';
 
 describe('PermissionDirective', () => {
+
+  let directive:PermissionDirective;
+  let mockElementRef:ElementRef;
+  let mockERenderer2: jasmine.SpyObj<Renderer2>;
+
+  beforeEach(() => {
+
+    /**
+     * ElementRef & Renderer2 cannot be injected directly
+     *    These are not services that Angular provides through DI.
+     *    ElementRef and Renderer2 are typically provided by Angular when used inside a component or directive.
+     */
+
+    mockElementRef = new ElementRef(document.createElement('div')); 
+    mockERenderer2 = jasmine.createSpyObj('Renderer2',['removeChild']);
+
+     TestBed.configureTestingModule({
+       providers:[
+        PermissionDirective,
+        { provide: ElementRef, useValue: mockElementRef },
+        { provide: Renderer2, useValue: mockERenderer2 }
+       ]
+     });
+     directive = TestBed.inject(PermissionDirective);
+  });
+
   it('should create an instance', () => {
-    const directive = new PermissionDirective();
     expect(directive).toBeTruthy();
+    expect(mockElementRef).toBeTruthy();
+    expect(mockERenderer2).toBeTruthy();
+  });
+
+  it('should remove element if permission not present', () => {
+    directive.permisstion = 11;
+    directive.ngAfterViewInit();
+    expect(mockERenderer2.removeChild).toHaveBeenCalledWith(mockElementRef.nativeElement.parentNode, mockElementRef.nativeElement);
+    // expect(mockERenderer2.removeChild).toHaveBeenCalled();
+  });
+
+  it('should not remove the element if permission is present', () => {
+    directive .permisstion = 10;
+    directive.ngAfterViewInit();
+    expect(mockERenderer2.removeChild).not.toHaveBeenCalled();
   });
 });
